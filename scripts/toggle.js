91407fa00adc45baba2d2a64cc7a81d9426f7cb6
lorence A. Tenorio <lorencetenorio24@gmail.com>
2026-02-16 22:41:11 +0800

    added id to different section and added toggle for survey question

---
+++ scripts/toggle.js
@@ -0,0 +1,6 @@
+function startSurvey() {
+            document.getElementById('mainSection').classList.add('hidden');
+            document.getElementById('commentSection').classList.add('hidden');
+
+            document.getElementById('surveyQuestionSection').classList.remove('hidden');
+        }
